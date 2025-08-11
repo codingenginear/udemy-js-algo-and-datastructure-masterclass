6232c06a27a9a7d357face87db98b5ae148707b6
codingenginear <momosawi12@gmail.com>
2025-08-11 12:37:58 -0400

    so this is my first try at creating the merge funct ion - works to a degree but has many issues - will fix these issues in the next iterations of this function

--- section15.js
+++ section15.js
@@ -2,27 +2,42 @@
 
 // My attempt at creating the merge function of the merge sort algorithm
 
-const sortedArray1 = [1,4,5,8];
-const sortedArray2 = [3,6,10,14];
+const sortedArray1 = [1, 4, 5, 8, 11, 20];
+const sortedArray2 = [3, 6, 10, 14];
 
 const merge = (arr1, arr2) => {
   var mergedArray = [];
   var j = 0;
+  var outsideI = 0;
 
   for (let i = 0; i < sortedArray1.length; i++) {
     if (sortedArray1[i] < sortedArray2[j]) {
       mergedArray.push(sortedArray1[i]);
+      outsideI++;
     }
     else if (sortedArray1[i] > sortedArray2[j]) {
       mergedArray.push(sortedArray2[j]);
       j++;
       i--;
+      outsideI--;
     }
-    if (i === sortedArray1.length - 1 && j !== sortedArray2.length - 1) {
+    console.log("Outside I: ", outsideI);
+    console.log("inside I: ", i);
+  }
+
+  if (outsideI === sortedArray1.length - 1 && j !== sortedArray2.length - 1) {
       const slicedArray = sortedArray2.slice(j);
       mergedArray = mergedArray.concat(slicedArray);
+      console.log("sliced Array 2");
+    
     }
-  }
+    else if (j === sortedArray2.length - 1 && outsideI !== sortedArray1.length - 1) {
+      const slicedArray = sortedArray1.slice(outsideI);
+      mergedArray = mergedArray.concat(slicedArray);
+      console.log("sliced Array 1");
+    
+    }
+
   console.log(mergedArray);
 }
 
