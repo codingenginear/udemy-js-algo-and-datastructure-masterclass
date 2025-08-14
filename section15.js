d73ab8bc8e7748f26f8edb847ce7aec7d8f0cf87
codingenginear <momosawi12@gmail.com>
2025-08-13 22:09:27 -0400

    made some progress on this merge function by rmeoving the < sign in the checks for when to slice and switch to using === signs because we don't want it to slice while i or j are smaller then the length of arrays we want it to slice the array when the other (i or j) reaches the end of the array

--- section15.js
+++ section15.js
@@ -8,36 +8,28 @@ const sortedArray2 = [3, 6, 10, 14];
 const merge = (arr1, arr2) => {
   var mergedArray = [];
   var j = 0;
-  var outsideI = 0;
 
-  for (let i = 0; i < arr1.length; i++) {
+  for (let i = 0; i < arr1.length && j < arr2.length; i++) {
     if (arr1[i] < arr2[j]) {
       mergedArray.push(arr1[i]);
-      outsideI++;
     }
     else if (arr1[i] > arr2[j]) {
       mergedArray.push(arr2[j]);
       j++;
       i--;
-      outsideI--;
     }
-    console.log("Outside I: ", outsideI);
-    console.log("inside I: ", i);
-  }
 
-  if (outsideI === arr1.length - 1 && j !== arr2.length - 1) {
+    if (i === arr1.length) {
       const slicedArray = arr2.slice(j);
       mergedArray = mergedArray.concat(slicedArray);
-      console.log("sliced Array 2");
-    
-    }
-    else if (j === arr2.length - 1 && outsideI !== arr1.length - 1) {
-      const slicedArray = arr1.slice(outsideI);
-      mergedArray = mergedArray.concat(slicedArray);
       console.log("sliced Array 1");
-    
     }
-
+    else if (j === arr2.length - 1) {
+      const slicedArray = arr2.slice(i);
+      mergedArray = mergedArray.concat(slicedArray);
+      console.log("sliced Array 2");
+    }
+  }
   console.log(mergedArray);
 }
 
