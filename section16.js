cf52290894bdba6824d7e62aad2fb55bf6eb3519
codingenginear <momosawi12@gmail.com>
2025-08-22 03:49:53 -0400

    got the first part of the pivot function working - where everything that is less than the pivot or everything that the pivot is greater than ends up infront of the pivot

--- section16.js
+++ section16.js
@@ -3,7 +3,8 @@
 // My attempt at creating the Pivot function of the Quick Sort algorithm
 
 // So we need a swap function that we have built earlier in the course and will use to swap 2 elements in an array to switch their places in the array.
-const array1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const array2 = [4, 8, 2, 1, 5, 7, 6, 3];
 
 // This is the original swap function. We will write a swap function in es6 syntax as well underneath it, they both do the same thing.
 const swap = (arr, indx1, indx2) => {
@@ -12,9 +13,22 @@ const swap = (arr, indx1, indx2) => {
   arr[indx2] = temp;
   return arr;
 };
-
-console.log(swap(array1, 0, 2));
+// console.log(swap(array1, 0, 8));
 
 const pivot = (arr, start = 0, end = arr.length - 1) => {
+  let swapIndx = start;
+  let pivot = arr[start];
 
+  for (let i = start + 1; i < arr.length - 1; i++) {
+    if (pivot > arr[i]) {
+      swapIndx++;
+      swap(arr, swapIndx, i);
+    }; 
+  };
+
+  
+  return arr;
 };
+
+console.log(pivot(array2));
+
