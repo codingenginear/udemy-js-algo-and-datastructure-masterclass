807e21298a7dc4196d1d9bd2cfdc72df3509d71f
codingenginear <momosawi12@gmail.com>
2025-10-13 18:51:57 -0400

    adding section 3 exercises to the code base

---
+++ section3.js
@@ -0,0 +1,14 @@
+// Analyzing Performance of Arrays and Objects
+
+const array = [];
+
+// Removing or Adding to the end is O(1) Performance
+array.push('1');
+array.push('2');
+
+// Removing or adding from beginning is O(n) Performance
+const variable = array.unshift();
+
+// Accessing is O(1) Performance
+const indexZero = array[0];
+
