89089ff2e73025b07afd534e780423c9d46756a7
codingenginear <momosawi12@gmail.com>
2025-10-13 19:08:43 -0400

    add some edge cases

--- section4.js
+++ section4.js
@@ -10,6 +10,7 @@ function charCount(string) {
 
   for (let i = 0; i < string.length; i++) {
     if (string[i] === ' ') continue;
+
     if (charObj[string[i]]) {
       charObj[string[i]] ++;
     }
@@ -17,7 +18,13 @@ function charCount(string) {
       charObj[string[i]] = 1;
     };
   };
+
   return charObj;
 };
 
 console.log(charCount('Hello World'));
+
+// Some more edge cases to worry about
+
+// e.g. 'Hello World 123123' // Should we ignore the lower and upper case or should we add lower case and uppercase seperately
+// e.g. 'Hello World 23423' // Should we ignore spacing and numbers etc.
