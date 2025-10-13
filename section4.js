fe27d3902f51ee51fa0281091cf83f2d7395e88e
codingenginear <momosawi12@gmail.com>
2025-10-13 19:02:04 -0400

    finished the exercise charCount fn for this section

---
+++ section4.js
@@ -0,0 +1,22 @@
+// Section 4 - Problem Solving Approach
+
+// Exercise
+// Write a fn which takes in a string and returns the count of each character in the string.
+
+// e.g charCount('abaaa') // {a: 4, b: 1}
+
+function charCount(string) {
+  let charObj = {};
+
+  for (let i = 0; i < string.length; i++) {
+    if (charObj[string[i]]) {
+      charObj[string[i]] ++;
+    }
+    else {
+      charObj[string[i]] = 1;
+    };
+  };
+  return charObj;
+};
+
+console.log(charCount('aaabbbcccddd'));
