77bce7f2cc3ab1cdc0d49b888f10cf62216e3999
codingenginear <momosawi12@gmail.com>
2025-10-13 17:19:06 -0400

    section 2 - vid 7 - timing our code - explanation and comments regarding code

--- section2.js
+++ section2.js
@@ -1,3 +1,10 @@
 // Section 2 - Big O Notiation
 
 // Video 7 - Timing Our Code
+
+// So in this video or section we are learning to time our code. We will first define a function which will take x amount of time. 
+// Then we will use the performance.now() function to take the time before and after the execution of the function. 
+// This gives us the time before the fn was executed and after execution also gives us the time after fn was executed. 
+// then we can just subtract the t1 from t2. This will give us how long the function took to execute.
+// e.g. if t1 runs @ 17:00:00 PM then we make dinner and after dinner we clock the time (t2) @ 17:30:00
+// then we know it took 30 mins to cook dinner.
