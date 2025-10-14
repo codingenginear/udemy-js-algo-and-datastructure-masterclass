54d6480aeab86550fb3ef09d6b9378e523ed0e86
codingenginear <momosawi12@gmail.com>
2025-10-14 14:24:46 -0400

    created alot ore refactors

--- section4.js
+++ section4.js
@@ -22,7 +22,7 @@ function charCount(string) {
   return charObj;
 };
 
-console.log(charCount('Hello World'));
+console.log('Character Count 1: ', charCount('Hello World'));
 
 // Some more edge cases to worry about
 
@@ -34,7 +34,7 @@ console.log(charCount('Hello World'));
 
 // First refactor handles and checks if the input is correct - it ignores all commas etc. and only allows alpha numeric characters like numbers and letters - no symbols
 
-function charCount(str) {
+function charCount2(str) {
   var result = {};
 
   for (var i = 0; i < str.length; i++) {
@@ -50,3 +50,53 @@ function charCount(str) {
   };
   return result;
 };
+
+console.log('Character Count 2: ', charCount2('Hello World 2342!@#!'));
+
+
+// Second refactor makes the function look a lot better aesthetically and also creates shorter logic for the if statement;
+
+function charCount3(str) {
+  var result = [];
+  for (var char of str) {
+    char = char.toLowerCase();
+    if (/[a-z0-9]/.test(char)) {
+      result[char] = ++result[char] || 1;
+    };
+  };
+  return result;
+};
+
+console.log('Character Count 3: ', charCount3('Hello World!!!'));
+
+
+// Third refactor takes into account the performance - it looks at whether the regex used in the check for valid char's is a good way or not 
+// - we can get a char code which gives us a number for any character 
+// and that could help us to create a range and check for valid characters within a given range in order to process it or not.
+
+function charCount4(str) {
+  var result = [];
+  for (var char of str) {
+    if (isAlphaNumeric(char)) {
+      char = char.toLowerCase();
+      result[char] = ++result[char] || 1;
+    }
+  };
+  return result;
+};
+
+function isAlphaNumeric(char) {
+  var code = char.charCode(0);
+  if 
+    (
+      !(code > 47 && code < 58) &&
+      !(code > 64 && code < 91) &&
+      !(code > 96 && code < 123)
+    ) {
+      return false;
+    };
+    
+  return true;
+};
+
+console.log('Character Count 4: ', charCount4('Hello World!!!'));
