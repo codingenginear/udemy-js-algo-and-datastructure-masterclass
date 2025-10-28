9ae4e3754595001aaf96e93637d575074af09d41
codingenginear <momosawi12@gmail.com>
2025-10-28 04:46:43 -0400

    starting another challenge called countUniqueValues - problem is from section 5 in the js algo and ds udemy course

--- section5.js
+++ section5.js
@@ -124,3 +124,15 @@ const parseStr = (str) => {
 // console.log(isAnagram('Hello World', 'Hello World'));
 // console.log(isAnagram('iceman', 'cinema'));
 console.log(isAnagram('texttimetwist', 'timetwisttext'));
+
+
+
+//-----------------------------------------------------
+
+
+// Video 33: Multiple Pointers Pattern: Count Unique Values Challenge
+
+// countUniqueValues
+// Implement a function called countUniqueValues, which accepts a sorted array, and counts the unique values in the array. There can be negative numbers in the array, but it will always be sorted.
+
+
