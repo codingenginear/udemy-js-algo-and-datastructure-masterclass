cda295f941d7f2bee45e71348ed7a6accdfcb9b8
codingenginear <momosawi12@gmail.com>
2025-12-13 19:48:52 -0500

    just making a commit

--- section5.js
+++ section5.js
@@ -200,6 +200,7 @@ const sumZero = (arr) => {
   };
 };
 
+
 console.log(sumZero([-4, -3, -2, -1, 0, 1, 2, 3, 5]))   // [-3, 3]
 console.log(sumZero([-2, 0, 1, 3]))    // undefined
 console.log(sumZero([1, 2, 3]))    // undefined
