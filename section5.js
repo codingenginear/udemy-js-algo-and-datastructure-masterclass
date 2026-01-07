924e685ad0b1b2f650d2d33112eca5fcab850684
codingenginear <momosawi12@gmail.com>
2026-01-06 23:14:27 -0500

    completed the solution for Video #35: Sliding Window Pattern: Max Subarray Sum Problem

--- section5.js
+++ section5.js
@@ -254,10 +254,29 @@
 // This pattern involves creating a window which can either be an array or number from on position to another. Depending on a certain condition, the window either increases or closes (and a new window is created). 
 // Useful for keeping track of a subset of data in an array/string etc.
 
+// Problem: Write a question called maxSubarraySum which accepts an array of integers and a number called n. The function should aclculate the maximum sum of n consecutive elements in the array.
 
 
-maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 2) // 10
-maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 4) // 17
+const maxSubarraySum = (arr, num) => {
+  let maxSum = 0;
+  let tempSum = 0;
+  if (arr.length < num) return null;
+
+  for (let i = 0; i < num; i++) {
+    maxSum += arr[i]
+  }
+  tempSum = maxSum;
+  for (let i = num; i < arr.length; i++) {
+    tempSum = tempSum - arr[i - num] + arr[i];
+    maxSum = Math.max(maxSum, tempSum);
+    console.log(maxSum);
+  }
+  return maxSum;
+} 
+
+
+// maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 2) // 10
+// maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 4) // 17
 maxSubarraySum([4, 2, 1, 6], 1) // 6
-maxSubarraySum([4, 2, 1, 6], 4) // 13
-maxSubarraySum([], 4) // null
+// maxSubarraySum([4, 2, 1, 6], 4) // 13
+// maxSubarraySum([], 4) // null
