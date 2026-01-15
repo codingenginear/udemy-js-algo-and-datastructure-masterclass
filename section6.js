425f9131c7f56eab07e0c424473e137e7707d276
codingenginear <momosawi12@gmail.com>
2026-01-14 22:52:32 -0500

    starting the first coding exercise in Section 6

--- section6.js
+++ section6.js
@@ -1,4 +1,20 @@
-//JS Algorithms and Data Structures Masterclass: Section 6: 100% Optional Challenges.
+// JS Algorithms and Data Structures Masterclass: Section 6: 100% Optional Challenges.
 
+// Coding Exercise #3:
+
+// Frequency Counter - sameFrequency
+
+// Write a function called sameFrequency. Given two positive integers, find out if the two numbers have the same frequency of digits.
+
+// Your solution MUST have the following complexities:
+
+// Time: O(N)
+
+// Sample Input:
+
+//     sameFrequency(182,281) // true
+//     sameFrequency(34,14) // false
+//     sameFrequency(3589578, 5879385) // true
+//     sameFrequency(22,222) // false
 
 //
