2f7d6931a40832c9a53aea5ed13ac6cadf0c8e4f
codingenginear <momosawi12@gmail.com>
2026-01-14 23:55:40 -0500

    wrote my pseudocode and beginning to actually solve the problem

--- section6.js
+++ section6.js
@@ -17,4 +17,21 @@
 //     sameFrequency(3589578, 5879385) // true
 //     sameFrequency(22,222) // false
 
-//
+// Solution: My Solution
+// Parse each number into an object key and  || create a sorted array of single numbers by dividing the number up into an array and sorting the array
+// If parsed into an object compare the value for each key. return true if both keys and values are matching exactly
+// If put into an array and sorted then just merge the array into 1 number again and check to see if it's the same number. Because it has to be exactly the same numbers if sorted from largest to smalles or smallest to largest.
+
+const sameFrequency = (num1, num2) => {
+  if (num1.length !== num2.length) return false;
+  
+  const parsedNum1 = {};
+  const parsedNum2 = {};
+  
+  // Convert numbers into string to loop over
+  for (let digit of num1)
+  
+
+
+}
+
