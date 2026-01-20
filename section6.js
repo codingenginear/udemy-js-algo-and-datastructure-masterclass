d7f046433807ae6401328846cf8f989053a00b8f
codingenginear <momosawi12@gmail.com>
2026-01-19 20:04:05 -0500

    started to solve findAllDuplicates problem

--- section6.js
+++ section6.js
@@ -256,3 +256,22 @@ function constructNote(message, letters) {
 
 // Solution: My Solution
 
+const findAllDuplicates = (array) => {
+  const parsedArray = {};
+
+  for (let i = 0; i < array.length; i++) {
+    if (parsedArray[i]) {
+      parsedArray[i]++;
+    }
+    else {
+      parsedArray[i] = 1;
+    };
+    console.log(parsedArray);
+  }
+
+
+};
+
+console.log(findAllDuplicates([4, 3, 2, 7, 8, 2, 3, 1])); // array with 2 and 3
+console.log(findAllDuplicates([4, 3, 2, 1, 0])); // []
+console.log(findAllDuplicates([4, 3, 2, 1, 0, 1, 2, 3])); // array with 3, 2, and 1
