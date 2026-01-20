fb2d0bb46898011bcf61dfb07c661f2f0474c02f
codingenginear <momosawi12@gmail.com>
2026-01-19 20:03:15 -0500

    start section 6: coding exercise #6: frequency counter - findAllDuplicates

--- section6.js
+++ section6.js
@@ -233,9 +233,26 @@ function constructNote(message, letters) {
   return true;
 };
 
-console.log(constructNote('aa', 'abc')) // false
-console.log(constructNote('abc', 'dcba')) // true
-console.log(constructNote('aabbcc', 'bcabcaddff')) // true
-console.log(constructNote('aabbcc', 'bcabccaddff')) // true
-console.log(constructNote('aabbcc', '')) // true
-console.log(constructNote('', 'aaa')) // false
+// console.log(constructNote('aa', 'abc')) // false
+// console.log(constructNote('abc', 'dcba')) // true
+// console.log(constructNote('aabbcc', 'bcabcaddff')) // true
+// console.log(constructNote('aabbcc', 'bcabccaddff')) // true
+// console.log(constructNote('aabbcc', '')) // true
+// console.log(constructNote('', 'aaa')) // false
+
+// ---------------------------------------------------------------------------------------------------------
+
+// Coding Exercise #6: Frequency Counter - findAllDuplicates
+
+// Frequency Counter - findAllDuplicates
+
+// Given an array of positive integers, some elements appear twice and others appear once. Find all the elements that appear twice in this array. Note that you can return the elements in any order.
+
+//     findAllDuplicates([4,3,2,7,8,2,3,1]) // array with 2 and 3
+//     findAllDuplicates([4, 3, 2, 1, 0]) // []
+//     findAllDuplicates([4, 3, 2, 1, 0, 1, 2, 3]) // array with 3, 2, and 1
+
+// Time Complexity - O(n)
+
+// Solution: My Solution
+
