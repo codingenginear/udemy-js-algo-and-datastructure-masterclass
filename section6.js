699d1edd3e365b4656a4abe9ca0019ae09fbd29d
codingenginear <momosawi12@gmail.com>
2026-01-25 22:28:16 -0500

    starting coding exercise #7: multiple pointers - averagePair problem

--- section6.js
+++ section6.js
@@ -286,3 +286,7 @@ const findAllDuplicates = (array) => {
 console.log(findAllDuplicates([4, 3, 2, 7, 8, 2, 3, 1])); // array with 2 and 3
 console.log(findAllDuplicates([4, 3, 2, 1, 0])); // []
 console.log(findAllDuplicates([4, 3, 2, 1, 0, 1, 2, 3])); // array with 3, 2, and 1
+
+// ---------------------------------------------------------------------------------------------------------
+
+// Coding Exercise #7: Multiple Pointers - averagePair
