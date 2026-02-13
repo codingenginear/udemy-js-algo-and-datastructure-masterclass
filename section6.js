a65f6b4373fc24d962e077a354e35a9373fbe7b6
codingenginear <momosawi12@gmail.com>
2026-02-13 06:38:05 -0500

    just added some spaces to the test arrays for easier readability

--- section6.js
+++ section6.js
@@ -455,7 +455,7 @@ const findPair = (array, target) => {
   }
 
   for (let i = 0; i < array.length; i++) {
-    if (parsedObj[array[i]] === 2 && target === 0) {
+    if (parsedObj[array[i]] === 2 && target === 0) { 
       return true;
     }
     if (parsedObj[array[i] - target] && target !== 0) {
@@ -467,13 +467,14 @@ const findPair = (array, target) => {
 };
 
 console.log(findPair([6, 1, 4, 10, 2, 4], 2)); // true
-console.log(findPair([8,6,2,4,1,0,2,5,13],1)); // true
-console.log(findPair([4,-2,3,10],-6)); // true
-console.log(findPair([6,1,4,10,2,4], 22)); // false
+console.log(findPair([8, 6, 2, 4, 1, 0, 2, 5, 13],1)); // true
+console.log(findPair([4, -2, 3, 10],-6)); // true
+console.log(findPair([6, 1, 4, 10, 2, 4], 22)); // false
 console.log(findPair([], 0)); // false
-console.log(findPair([5,5], 0)); // true
-console.log(findPair([-4,4], -8)); // true
-console.log(findPair([-4,4], 8)); // true
-console.log(findPair([1,3,4,6],-2)); // true
-console.log(findPair([0,1,3,4,6],-2)); // true
-console.log(findPair([1,2,3], 0)); // false
+console.log(findPair([5, 5], 0)); // true
+console.log(findPair([-4, 4], -8)); // true
+console.log(findPair([-4, 4], 8)); // true
+console.log(findPair([1, 3, 4, 6],-2)); // true
+console.log(findPair([0, 1, 3, 4, 6],-2)); // true
+console.log(findPair([1, 2, 3], 0)); // false
+console.log(findPair([1, 2, 3, 3], 0)); // true
