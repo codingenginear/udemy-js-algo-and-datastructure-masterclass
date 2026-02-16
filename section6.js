82374178a650092224776580f5b9693e59a59749
codingenginear <momosawi12@gmail.com>
2026-02-16 16:58:00 -0500

    so i solved the issue to a degree but i am getting the wrong answer for a couple of them - will have to figure out what the issue is

--- section6.js
+++ section6.js
@@ -504,8 +504,8 @@ const findPair = (array, target) => {
 //  My Solution: 
 
 const maxSubarraySum = (array, num) => {
-  let highestNum;
-  let currentNum;
+  let highestNum = 0;
+  let currentNum = 0;
 
 
   for (let i = 0; i < array.length; i++) {
@@ -514,18 +514,16 @@ const maxSubarraySum = (array, num) => {
     }
 
     if (currentNum > highestNum) {
-    highestNum = currentNum;
-    currentNum = 0;
+      highestNum = currentNum;
+      currentNum = 0;
     }
-  }
-  
-  return highestNum;
+  };
 
+  return highestNum;
 };
                                       
 console.log(maxSubarraySum([100, 200, 300, 400], 2)) // 700
-
-// console.log(maxSubarraySum([1, 4, 2, 10, 23, 3, 1, 0, 20], 4))  // 39 
-// console.log(maxSubarraySum([-3, 4, 0, -2, 6, -1], 2)) // 5
-// console.log(maxSubarraySum([3, -2, 7, -4, 1, -1, 4, -2, 1], 2)) // 5
-// console.log(maxSubarraySum([2, 3], 3)) // null
+console.log(maxSubarraySum([1, 4, 2, 10, 23, 3, 1, 0, 20], 4))  // 39 
+console.log(maxSubarraySum([-3, 4, 0, -2, 6, -1], 2)) // 5
+console.log(maxSubarraySum([3, -2, 7, -4, 1, -1, 4, -2, 1], 2)) // 5
+console.log(maxSubarraySum([2, 3], 3)) // null
