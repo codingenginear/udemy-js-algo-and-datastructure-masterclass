4703b74589031cac6fb64af27d649f5a62441f28
codingenginear <momosawi12@gmail.com>
2026-02-16 16:52:12 -0500

    i think i solved coding exercise #10 - will now test it out

--- section6.js
+++ section6.js
@@ -506,19 +506,25 @@ const findPair = (array, target) => {
 const maxSubarraySum = (array, num) => {
   let highestNum;
   let currentNum;
-  for (let i = 0; i < num; i++) {
-    currentNum += array[i];
+
+
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < num; j++) {
+      currentNum += array[i + j];
+    }
+
+    if (currentNum > highestNum) {
+    highestNum = currentNum;
+    currentNum = 0;
+    }
   }
   
-  if (currentNum > highestNum) {
-    highestNum = currentNum;
-  }
-
   return highestNum;
 
 };
-
+                                      
 console.log(maxSubarraySum([100, 200, 300, 400], 2)) // 700
+
 // console.log(maxSubarraySum([1, 4, 2, 10, 23, 3, 1, 0, 20], 4))  // 39 
 // console.log(maxSubarraySum([-3, 4, 0, -2, 6, -1], 2)) // 5
 // console.log(maxSubarraySum([3, -2, 7, -4, 1, -1, 4, -2, 1], 2)) // 5
