1202c311858f42750af097f9832ce352c32a8b40
codingenginear <momosawi12@gmail.com>
2026-02-20 16:38:03 -0500

    solved the exercise #10 - sliding window - maxSubarraySum problem - the issue was i wasn't clearing and zeroing out the currentNum if the currentNum was ever less than the highestNum - i was only zeroing it out when currentNum was larger - this means that currentNum was just accumulating and would eventually end up being larger than highestNum when it wasn't supposed to because it was never getting to 0 in the window that it summed to something smaller than highestNum - now that i zeroed out the currentNum for every cycle of j - regardless if currentNum was larger than highestNum or not it got fixed - this means currentNum will never accumulate and for every cycle of j when j or the sliding window reaches it's end currentNum restarts from 0 again and begins to accumulate from new - this solves the issue

--- section6.js
+++ section6.js
@@ -517,6 +517,9 @@ const maxSubarraySum = (array, num) => {
       highestNum = currentNum;
       currentNum = 0;
     }
+    else {
+      currentNum = 0;
+    }
   };
 
   return highestNum;
@@ -526,5 +529,4 @@ console.log(maxSubarraySum([100, 200, 300, 400], 2)) // 700
 console.log(maxSubarraySum([1, 4, 2, 10, 23, 3, 1, 0, 20], 4))  // 39 
 console.log(maxSubarraySum([-3, 4, 0, -2, 6, -1], 2)) // 5
 console.log(maxSubarraySum([3, -2, 7, -4, 1, -1, 4, -2, 1], 2)) // 5
-
 console.log(maxSubarraySum([2, 3], 3)) // null
